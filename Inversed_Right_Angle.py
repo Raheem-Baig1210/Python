ed70bcc861eedcea91d4aa023e876582b73212d9
Raheem-Baig1210 <raheembaig822@gmail.com>
2025-02-22 18:27:59 +0530

    Star_Shaped

---
+++ Inversed_Right_Angle.py
@@ -0,0 +1,3 @@
+a=int(input("Enter the value of a : "))
+for i in range(a,0,-1):
+    print('*' * i)
---
+++ Pyramid.py
@@ -0,0 +1,3 @@
+a=int(input("Enter the value of a :"))
+for i in range(a):
+    print(' '*(a-i-1)+"*"*(2*i+1))
